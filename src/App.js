fba015a0452c92143f0b5319f4c7f31f787c615e
re8code <won@re8code.com>
2024-10-12 23:33:11 +0900

    final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './route/Home.js';
+import Page1 from './route/Page1.js';
+import Page2 from './route/Page2.js';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/page1' element={<Page1 />} />
+        <Route path='/page2' element={<Page2 />} />
       </Routes>
     </BrowserRouter>
   );
--- src/route/Page1.js
+++ src/route/Page1.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 const element = () => {
   return (
     <>
-      <h1>Currect Page: Home</h1>
+      <h1>Currect Page: Page 1</h1>
       <hr />
-      <img src='./img1.png' alt='1st image' />
       <img src='./img2.png' alt='2nd image' />
       <img src='./img3.png' alt='3rd image' />
+      <img src='./img1.png' alt='1st image' />
       <h2>Move Page</h2>
       <div>
-        prev page: <Link to='/page2'>Page 2</Link>
+        prev page: <Link to='/'>Home</Link>
         <br />
-        next page: <Link to='/page1'>Page 1</Link>
+        next page: <Link to='/page2'>Page 2</Link>
       </div>
     </>
   );
